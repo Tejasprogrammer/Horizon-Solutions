fc47ae1f0dca6a99600ac1054f4d8e1a6e0b1370
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2025-01-30 20:35:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple script to show an alert when the "Get in Touch" button is clicked
+document.querySelector('.cta-btn').addEventListener('click', function() {
+  alert("Thank you for contacting Horizon Solution! We'll get back to you soon.");
+});
